d534d9d00e8bcd2d847c1dafb8ed3cc6b8ff3ce9
olyagoroshko <olga.og.goroshko@gmail.com>
2020-06-24 19:16:14 +0300

    header__nav

---
+++ js/header-select.js
@@ -0,0 +1,24 @@
+const selectAllElements = document.querySelectorAll("[data-select]");
+
+selectAllElements.forEach(function (item) {
+    item.addEventListener("click", function () {
+        const realSelect = this.nextElementSibling;
+
+        if (event.target.hasAttribute("data-select-item")) {
+
+            const itemTitle = event.target.getAttribute("data-select-item");
+
+            this.querySelector("[data-select-title]").textContent = itemTitle;
+            this.querySelector(".header-select__dropdown").classList.toggle("hidden");
+
+            if (realSelect) {
+
+                realSelect.value = itemTitle;
+
+            }
+
+        } else {
+            this.querySelector(".header-select__dropdown").classList.toggle("hidden")
+        }
+    })
+})
